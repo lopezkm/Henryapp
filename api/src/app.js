a8f5e6c3ebc58d23f5780088d9db879807ec7f51
Felipe Taboada <ftaboada@gmail.com>
2020-11-05 20:51:33 -0300

    schema

--- api/src/app.js
+++ api/src/app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const graphqlHTTP = require("express-graphql");
-
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 const server = express();
 
 server.name = "HENRYAPP";
 
 server.use(morgan("tiny"));
 server.use(cors());
-
+server.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 module.exports = server;
