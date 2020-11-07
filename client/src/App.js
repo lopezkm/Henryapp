19c356c09a4457811a3f5e91944b30ac68b8ccf2
Matias Lopez Klyver <lopezk_matias@hotmail.com>
2020-11-07 19:44:01 -0300

    component DashBoard added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
 
-import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import LandingPage from './components/home/landingPage'
-import Home from './components/home/home'
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import LandingPage from './components/home/landingPage.jsx';
+import Home from './components/home/home.jsx';
+import DashBoard from './components/dashBoard.jsx';
 
 
 function App() {
   return (
     <Router>
 
-      <Route
-      exact path='/'
-      render={()=> <LandingPage/> }
-      />
+      <Route exact path='/' render={()=> <LandingPage/> }/>
       
-      <Route
-      exact path='/home'
-      render={()=> <Home /> }
-      />
+      <Route exact path='/home' render={()=> <Home/> }/>
+
+      <Route exact path='/dashboard' render={()=> <DashBoard/> }/>
 
     </Router>
   );
