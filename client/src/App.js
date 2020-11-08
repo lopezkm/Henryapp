374d5b5daaa73e9df9b3086083bcce29fea469aa
omar zarate <8omarzarate@gmail.com>
2020-11-08 12:33:48 -0300

    "more bugs"

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
 
-<<<<<<< HEAD
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import LandingPage from './components/home/landingPage'
 import UserProfile from './components/userProfile'
 import React from 'react'
-=======
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import LandingPage from './components/home/landingPage.jsx';
-import Home from './components/home/home.jsx';
-import DashBoard from './components/dashBoard.jsx';
->>>>>>> main
 
 
 function App() {
   return (
     <div>
-      <>
+    
     <Router>
-<<<<<<< HEAD
       
       <Route
       exact path='/'
@@ -29,17 +20,8 @@ function App() {
       component={UserProfile}
       />
       
-=======
-
-      <Route exact path='/' render={()=> <LandingPage/> }/>
-      
-      <Route exact path='/home' render={()=> <Home/> }/>
-
-      <Route exact path='/dashboard' render={()=> <DashBoard/> }/>
-
->>>>>>> main
     </Router>
-    </>
+    
       </div>
   );
 }
