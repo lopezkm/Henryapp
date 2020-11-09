158ece6187736ec2d522198716da2a9ed6567a59
Cinthia Daira Pardos <cin@pop-os.localdomain>
2020-11-09 15:36:32 -0300

    footer

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,63 +1,49 @@
-import React from "react";
-import { FaFacebook } from "react-icons/fa";
-import { GrInstagram } from "react-icons/gr";
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 import '../../css/footer.css'
-import { AiFillTwitterCircle } from "react-icons/ai";
-import { Link } from "react-router-dom";
-//import Logo from  '../home/images/logohen.png';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+   theme.palette.grey[900],
+  },
+  typography:{
+    allVariants: {
+      color: "white"
+    },
+  }
+}));
 
-function Footer() {
+export default function Footer() {
+  const classes = useStyles();
+
   return (
-    <div className="footer-container">
-      <div className="d-flex justify-content-center">
-        <div className="mt-5 footer-content">
-          <div>
-            <img
-              className="image-brand mt-4"
-            
-              alt="Logo"
-            />
-          </div>
-          <div className="mt-5">
-            <h4 className="text-center ">Explore</h4>
-              <h6 className="text-center link-w mt-4">Home</h6>
-
-              <h6 className="text-center link-w mt-1">About</h6>
-          </div>
-          <div className="mt-5">
-            <h4 className="text-center">Follow</h4>
-            <h6 className="text-center mt-4 link-w"><a href="https://www.facebook.com/" className='a-about'>Facebook</a></h6>
-            <h6 className="text-center mt-1 link-w"><a href="https://www.instagram.com/" className='a-about'>Instagram</a></h6>
-            <h6 className="text-center mt-1 link-w"><a href="https://twitter.com/?lang=en" className='a-about'>Twitter</a></h6>
-          </div>
-          <div className="mt-5">
-            <h4 className="text-center">Legal</h4>
-              <h6 className="text-center link-w mt-4">Terms</h6>
-              <h6 className="text-center link-w mt-1">Privacy</h6>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="d-flex justify-content-center mt-5">
-          <div className="d-flex m-2">
-            <h6 className="mr-3 h2"><a href="https://www.facebook.com/" className='a-about'>
-              <FaFacebook /></a>
-            </h6>
-            <h6 className="h2"><a href="https://www.instagram.com/" className='a-about'>
-              <GrInstagram /></a>
-            </h6>
-            <h6 className="ml-3 h2"><a href="https://twitter.com/?lang=en" className='a-about'>
-              <AiFillTwitterCircle /></a>
-            </h6>
-          </div>
-        </div>
-        <div className="d-flex justify-content-center">
-          <h6 className="mb-5">Copyright. All rights reserved.</h6>
-        </div>
-      </div>
+    <div className={classes.root}>
+      <CssBaseline />
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <ul style={{color:'white'}}>
+          <li>Info sobre Henry AQUI</li>
+          <li>Contacto a redes</li>
+          <li >Contacto a redes</li>
+          </ul>
+        </Container>
+      </footer>
     </div>
   );
 }
-
-export default Footer;
