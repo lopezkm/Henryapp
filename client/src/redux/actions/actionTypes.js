7f795ae4f5f801354c48574bb1a0d8e71675ce66
omar zarate <8omarzarate@gmail.com>
2020-11-10 18:04:34 -0300

    carpetas y actionTypes

---
+++ client/src/redux/actions/actionTypes.js
@@ -0,0 +1,16 @@
+//COHORTE ACTIONS
+export const GET_COHORT= "GET_COHORT";
+export const GET_COHORTS= "GET_COHORTS";
+export const CREATE_COHORT= "CREATE_COHORT";
+export const PUT_COHORT= "PUT_COHORT";
+export const DELETE_COHORT= "DELETE_COHORT";
+
+//STUDENTS ACTIONS
+export const GET_USERS= "GET_USERS";
+export const GET_USER= "GET_USER";
+export const POST_USER= "POST_USER";  /*Register, crea un usuario*/
+export const UPDATE_USER= "UPDATE_USER";
+
+
+
+
