1e1158843e64d79cdc65e26aded67db403337f69
Alfredo <nzozez@gmail.com>
2020-11-10 12:18:10 -0300

    error Solucionado

--- api/src/graphql/resolvers/sprint.js
+++ api/src/graphql/resolvers/sprint.js
@@ -1,10 +1,13 @@
-import { Sprint } from '../../models/Sprint';
-import root from '../typeDefs/root';
+import { Sprint } from '../../models';
 
 export default {
     Query: {
-        sprints: async () => await Sprint.find(),
-        sprint: async (_, args) => await Sprint.findById(args.id)
+        sprints: async () =>{
+            return await Sprint.find()
+        }, 
+        sprint: async (_, args) => {
+            return await Sprint.findById(args.id)
+        }
     },
     Mutation: {
         // Creamos una sprint
@@ -13,12 +16,12 @@ export default {
             const sprint = await Sprint.findOne({
                 name: args.name
             });
-            if( name ){
+            if( sprint ){
                 throw new Error('Sprint ya cargada')
             }
             // si No Existe
             const newSprint = await Sprint.create(args);
-            return newSprint
+            return newSprint;
         }
     }
 }
