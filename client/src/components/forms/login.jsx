88cbf402bd924cc0fbc6e09781ea2d45ff50d007
Cinthia Daira Pardos <cin@pop-os.localdomain>
2020-11-10 14:18:42 -0300

    styles separated

--- client/src/components/forms/login.jsx
+++ client/src/components/forms/login.jsx
@@ -8,45 +8,12 @@ import {Link} from 'react-router-dom';
 import '../../css/forms.css'
 import  useForm from './useForm'
 import validate from './validateLogin'
+import useStyleslog from './stylesLogin' //import styles
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
-    },
-    main: {
-      marginTop: theme.spacing(3),
-      height: '100vh',
-      backgroundColor: theme.palette.secondary.main,
-    },
-      button: {
-        marginTop: theme.spacing(8),
-        marginLeft: theme.spacing(7),
-      },
-      image: {
-        backgroundImage: 'url(https://source.unsplash.com/random)',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor:
-          theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      },
-      paper: {
-        margin: theme.spacing(8, 4),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      },
-      form: {
-        width: '90%', 
-        marginTop: theme.spacing(12),
-        marginLeft: theme.spacing(12),
-        },
-    }));
+
 
 export default function Login() {
- const classes = useStyles();
+ const classes = useStyleslog();
  const {  values, handleChange, handleSubmit,  errors 
 }= useForm(submit, validate);
 
--- client/src/components/forms/register2.jsx
+++ client/src/components/forms/register2.jsx
@@ -6,44 +6,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import {Link} from 'react-router-dom';
 import '../../css/forms.css'
-import  useForm from './useForm'
-import validate from './validateLogin'
+import  useForm from './useForm' //standard hooks for forms
+import validate from './validateLogin' //validations
+import useStyles from './stylesRegister' //import styles
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
-    },
-    main: {
-      marginTop: theme.spacing(3),
-      height: '100vh',
-      backgroundColor: theme.palette.secondary.main,
-    },
-      button: {
-        marginTop: theme.spacing(3),
-        marginLeft: theme.spacing(7),
-      },
-      image: {
-        backgroundImage: 'url(https://source.unsplash.com/random)',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor:
-          theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      },
-      paper: {
-        margin: theme.spacing(8, 4),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      },
-      form: {
-        width: '70%', 
-        marginTop: theme.spacing(5),
-        marginLeft: theme.spacing(12),
-        },
-    }));
 
 export default function Register2() {
  const classes = useStyles();
